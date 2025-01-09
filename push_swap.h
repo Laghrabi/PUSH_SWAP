85f05c40e71ed1fea57603bd7cff010cc4460bcd
Chaimaa Laghrabi <claghrab@e1r7p3.1337.ma>
2025-01-09 18:37:34 +0100

    [ADD] Header file

---
+++ push_swap.h
@@ -0,0 +1,26 @@
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+/*INCLUDES*/
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <limits.h>
+# include <stdint.h>
+
+/*STRUCTURE*/
+
+typedef struct t_stack {
+    int *data;
+    struct t_stack *next;
+} t_stack;
+
+/*FUNCTIONS PROTOTYPE*/
+
+void    swap(t_stack *stack);
+void    sa(t_stack **stackA);
+void    sb(t_stack **stackB);
+void    ss(t_stack **stackA, t_stack **stackB);
+
+#endif

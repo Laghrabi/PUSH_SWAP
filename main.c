98a6b59c625525f859b0ef7793387a02e0581413
Laghrabi <laghrabichaimae>
2025-01-22 19:57:35 +0100

    [TEST] Output of stackA

--- main.c
+++ main.c
@@ -4,7 +4,6 @@ int main(int ac, char **av)
 {
     char    **chunks;
     t_stack *stackA;
-    //t_stack *stackB;
 
     chunks = NULL;
     if (ac < 2)
@@ -12,20 +11,11 @@ int main(int ac, char **av)
     if (!check_input(ac, av, &chunks))
         exit_error(NULL, NULL);
     stackA = fill_stack(chunks);
-    // if (chunks != NULL)
-    // {
-    //     stackA = fill_stack(chunks);
-    //     free_splited(chunks);
-    // }
-    // else
-    //     stackA = fill_stack(av + 1);
     printf("Stack A:\n");
     while (stackA)
     {
         printf("[%d]\n", stackA->data);
         stackA = stackA->next;
     }
-    clean_stack(&stackA);
-    //system("leaks a.out");
     return (0);
 }
